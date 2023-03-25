01b91d1472a807d37c1002db53fe0ed2c5c917fc
Amal Salvin Joseph <amalsalvin97@gmail.com>
2023-03-25 07:24:08 +0530

    allSettled is supported only from ECMAScript 2020

--- app/utils/common.ts
+++ app/utils/common.ts
@@ -1,20 +1,20 @@
 import RNFS from 'react-native-fs';
 
 export const unlinkTmpFiles = (tempFilePaths: Array<string>) => {
-  // Promise.all(tempFilePaths.map(tempFilePath => RNFS.unlink(tempFilePath)))
-  // .then(() => {
-  //   console.log('All temporary files deleted successfully');
-  // })
-  // .catch(error => {
-  //   console.log('Error deleting temporary files: ', error);
-  // });
-
-  Promise.allSettled(tempFilePaths.map(tempFilePath => RNFS.unlink(tempFilePath)))
-  .then(results => {
-    const successfulDeletions = results.filter(result => result.status === 'fulfilled');
-    console.log(`${successfulDeletions.length} temporary files deleted successfully`);
+  Promise.all(tempFilePaths.map(tempFilePath => RNFS.unlink(tempFilePath)))
+  .then(() => {
+    console.log('All temporary files deleted successfully');
   })
   .catch(error => {
     console.log('Error deleting temporary files: ', error);
   });
+
+  // Promise.allSettled(tempFilePaths.map(tempFilePath => RNFS.unlink(tempFilePath)))
+  // .then(results => {
+  //   const successfulDeletions = results.filter(result => result.status === 'fulfilled');
+  //   console.log(`${successfulDeletions.length} temporary files deleted successfully`);
+  // })
+  // .catch(error => {
+  //   console.log('Error deleting temporary files: ', error);
+  // });
 }
