e87cd7d25b659ba8785b712085707438b6fe33d3
Amal Salvin Joseph <amalsalvin97@gmail.com>
2023-03-25 08:01:11 +0530

    unlinkTmpFiles refactor to async/await

--- app/utils/common.ts
+++ app/utils/common.ts
@@ -1,20 +1,14 @@
 import RNFS from 'react-native-fs';
 
-export const unlinkTmpFiles = (tempFilePaths: Array<string>) => {
-  Promise.all(tempFilePaths.map(tempFilePath => RNFS.unlink(tempFilePath)))
-  .then(() => {
-    console.log('All temporary files deleted successfully');
-  })
-  .catch(error => {
-    console.log('Error deleting temporary files: ', error);
-  });
+export const unlinkTmpFiles = async (tempFilePaths: Array<string>) => {
+  try {
+    await Promise.all(tempFilePaths.map(tempFilePath => {
+      console.log("File Name", tempFilePath)
+      return RNFS.unlink(tempFilePath)
+    }))
 
-  // Promise.allSettled(tempFilePaths.map(tempFilePath => RNFS.unlink(tempFilePath)))
-  // .then(results => {
-  //   const successfulDeletions = results.filter(result => result.status === 'fulfilled');
-  //   console.log(`${successfulDeletions.length} temporary files deleted successfully`);
-  // })
-  // .catch(error => {
-  //   console.log('Error deleting temporary files: ', error);
-  // });
+    console.log('All temporary files deleted successfully');
+  } catch(error) {
+    console.log('Error deleting temporary files: ', error);
+  };
 }
