0412c963b6180191fee1ed0630487052f9061724
Amal Salvin Joseph <amalsalvin97@gmail.com>
2023-04-08 01:02:04 +0530

    ReCompute Environment for MST Model as data is restored

--- app/models/root-store/setup-root-store.ts
+++ app/models/root-store/setup-root-store.ts
@@ -24,15 +24,24 @@ export async function createEnvironment(url: string | undefined = undefined) {
 /**
  * Setup the root state.
  */
-export async function setupRootStore(url: string | undefined = undefined) {
+export async function setupRootStore(url: string = undefined) {
   let rootStore: RootStore
   let data: any
+  let env: Environment
 
   // prepare the environment that will be associated with the RootStore.
-  const env = await createEnvironment(url)
+  // const env = await createEnvironment(url)
+  env = await createEnvironment(url)
+
   try {
     // load data from storage
     data = (await storage.load(ROOT_STATE_STORAGE_KEY)) || {}
+
+    // recompute and override the environment if url restored from storage
+    const storeUrl = data.appConfig.configURL.length && data.appConfig.configURL
+    url = url || storeUrl
+    env = await createEnvironment(url)
+
     rootStore = RootStoreModel.create(data, env)
   } catch (e) {
     // if there's any problems loading, then let's at least fallback to an empty state
