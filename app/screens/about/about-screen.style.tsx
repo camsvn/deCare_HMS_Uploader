89409fabcd1af0c35ca89d09badff9bc5ee46781
Amal Salvin Joseph <amalsalvin97@gmail.com>
2023-04-10 05:13:20 +0530

    about component init

---
+++ app/screens/about/about-screen.style.tsx
@@ -0,0 +1,133 @@
+import { ViewStyle, TextStyle } from "react-native"
+import { color, spacing, typography } from "../../theme"
+
+export const FULL: ViewStyle = { flex: 1, backgroundColor: color.background }
+
+export const BORDER: ViewStyle = { borderColor: color.errorRed, borderWidth: 3 }
+
+export const CONTAINER: ViewStyle = {
+  // flex: 0.5,
+  marginHorizontal: spacing[4]
+}
+
+export const TEXT: TextStyle = {
+  color: color.palette.white,
+  fontFamily: typography.primary,
+}
+
+export const BOLD: TextStyle = { fontWeight: "bold" }
+export const MARGIN_BOTTOM: TextStyle = { marginBottom: spacing[2] }
+
+export const CENTER: TextStyle = { textAlign: "center" }
+
+export const TITLE: TextStyle = {
+  ...TEXT,
+  ...BOLD,
+  color: color.text,
+  fontSize: 40,
+}
+
+export const HEADER: TextStyle = {
+  paddingTop: spacing[3],
+  paddingBottom: spacing[3] + spacing[1],
+  paddingHorizontal: 0,
+  backgroundColor: color.primary,
+  borderBottomLeftRadius: 5,
+  borderBottomRightRadius: 5,
+  zIndex: 0
+}
+
+export const HEADER_VIEW: ViewStyle = { 
+  position: "relative", 
+  top: 10, 
+  zIndex: 1
+}
+
+export const HEADER_TITLE: TextStyle = {
+  ...TEXT,
+  ...BOLD,
+  fontSize: 28,
+  // lineHeight: 38,
+  textAlign: "center",
+  letterSpacing: 1.5
+}
+
+export const CONTENT: TextStyle = {
+  ...TEXT,
+  // color: color.dimText,
+  color: color.primary,
+  fontSize: 14,
+  letterSpacing: 0.3,
+  lineHeight: 20
+  // lineHeight: 22
+}
+
+export const PRIMARY_CONTENT: TextStyle = {
+  fontSize: 22,
+  fontWeight: "bold",
+  marginTop: spacing[3]
+}
+
+export const MAINVIEW_CONTAINER: ViewStyle = { marginHorizontal: spacing[0] }
+export const LOGOVIEW_CONTAINER: ViewStyle = {
+  flex: 1,
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "row",
+}
+
+export const LOGO_CONTAINER: ViewStyle = {
+  width: 60,
+  height: 60,
+}
+
+export const FORM_CONTAINER: ViewStyle = {
+  flex: 1.25,
+  marginHorizontal: spacing[5],
+  alignItems: "center",
+}
+
+export const SUBMIT_BUTTON: ViewStyle = {
+  height: 45,
+  width: "100%",
+  borderRadius: 5,
+  marginTop: spacing[5],
+  backgroundColor: color.primary,
+}
+
+export const TEXT_FIELD: TextStyle = {
+  height: 30,
+  width: "100%",
+  borderRadius: 5,
+  marginHorizontal: spacing[0],
+  textAlignVertical: "top",
+  borderColor: color.palette.black,
+  borderWidth: 0.9,
+  marginVertical: spacing[3],
+  marginBottom: spacing[4],
+}
+
+export const BUTTON_CONTAINER: ViewStyle = {
+  paddingHorizontal: 0,
+  flexDirection: "row",
+  alignItems: "center"
+}
+
+export const LOGOUT_BUTTON_CONTAINER: ViewStyle = {
+  ...BUTTON_CONTAINER,
+  justifyContent: "flex-start"
+}
+
+export const LOGOUT_TEXT: TextStyle = {
+  paddingVertical: spacing[4]
+}
+
+export const LOGOUT_ICON: ViewStyle = {
+  transform: [{ rotate: '180deg' }],
+  marginLeft: 5
+}
+
+export const BUTTON_SPACING_CONTENT: ViewStyle = {
+  paddingVertical: spacing[4],
+  flex: 1
+}
---
+++ app/screens/about/about-screen.tsx
@@ -0,0 +1,100 @@
+import React, { FC, useState } from "react"
+import { Linking, View } from "react-native"
+import { StackScreenProps } from "@react-navigation/stack"
+import { observer } from "mobx-react-lite"
+import {
+  Button,
+  Header,
+  Screen,
+  Text,
+  Divider,
+  Icon
+} from "../../components"
+import { color, spacing} from "../../theme"
+import { NavigatorParamList } from "../../navigators"
+
+import { useStores } from "../../models"
+import * as settingScreenStyles from "./about-screen.style";
+import { showMessage } from "react-native-flash-message"
+
+
+export const AboutScreen: FC<StackScreenProps<NavigatorParamList, "about">> = observer(
+  ({ navigation }) => {
+    const phoneNumber = '+918086358930';
+    const websiteUrl = 'https://www.decare.team';
+
+    const handlePhonePress = () => {
+      Linking.openURL(`tel:${phoneNumber}`);
+    };
+
+    const handleWebsitePress = () => {
+      Linking.openURL(websiteUrl);
+    };
+
+    const goBack = () => {
+      console.log("Handle Back Click")
+      if (navigation.canGoBack()) {
+        navigation.goBack()
+      }
+      else{
+        showMessage({
+          message: `Can't go back`,
+          type: "info",
+          backgroundColor: color.dim,
+          color: color.primary
+        })
+      }
+    }
+
+    return (
+      <View testID="SettingScreen" style={settingScreenStyles.FULL}>
+        <Header
+         leftIcon="arrowLeft"
+         headerTx="common.header" 
+         leftIconStyle={{zIndex: 1, top:20}}
+         leftIconSize={20}
+         style={settingScreenStyles.HEADER} 
+         titleStyle={settingScreenStyles.HEADER_TITLE}
+         onLeftPress={goBack}
+        />
+        {/* <View style={settingScreenStyles.HEADER_VIEW}>
+          <Button onPress={() => goBack()} preset="link" type="highlight">
+            <Icon icon="back" width={30} height={30}/>
+          </Button>
+        </View> */}
+        <Screen style={settingScreenStyles.CONTAINER} backgroundColor={color.transparent} preset="scroll">
+          <View style={settingScreenStyles.MAINVIEW_CONTAINER}>
+
+            <Text style={settingScreenStyles.PRIMARY_CONTENT}>Terms of Sevice</Text>
+            <Divider color={color.primary} style={settingScreenStyles.MARGIN_BOTTOM}/>            
+            <Text tx="aboutScreen.contentParaLicense" style={[settingScreenStyles.CONTENT, {marginVertical: spacing[2]}]} />
+            
+            
+            <Text style={settingScreenStyles.PRIMARY_CONTENT}>About Us</Text>
+            <Divider color={color.primary} style={settingScreenStyles.MARGIN_BOTTOM}/>
+            <Text tx="aboutScreen.contentParaIntro" style={[settingScreenStyles.CONTENT, settingScreenStyles.MARGIN_BOTTOM]} />
+            <Text tx="aboutScreen.contentParaTwo" style={[settingScreenStyles.CONTENT, settingScreenStyles.MARGIN_BOTTOM]} />
+            <Text tx="aboutScreen.contentParaThree" style={[settingScreenStyles.CONTENT, settingScreenStyles.MARGIN_BOTTOM]} />
+            {/* <Text tx="aboutScreen.contentParaFinale" style={[settingScreenStyles.CONTENT, settingScreenStyles.MARGIN_BOTTOM]} /> */}
+
+            <Text style={settingScreenStyles.PRIMARY_CONTENT}>Contact Us</Text>
+            <Divider color={color.primary} style={settingScreenStyles.MARGIN_BOTTOM}/>            
+            <Text tx="aboutScreen.contentParaFinale" style={[settingScreenStyles.CONTENT, {marginVertical: spacing[2]}]} />
+            
+            <Text style={[settingScreenStyles.CONTENT]}>
+              call us at {' '}
+              <Text text="+91 80863 58930" style={[settingScreenStyles.CONTENT, { textDecorationLine: 'underline' }]} onPress={handlePhonePress}/>
+            </Text>
+            <Text text="or" />
+            <Text style={[settingScreenStyles.CONTENT]}>
+              visit our {' '}
+              <Text text="website (decare.team)" style={[settingScreenStyles.CONTENT, { textDecorationLine: 'underline' }]} onPress={handleWebsitePress}/>
+            </Text>
+
+            <Text tx="aboutScreen.contentParaCopyright" style={[settingScreenStyles.CONTENT, settingScreenStyles.CENTER, {fontStyle:"italic", marginTop: spacing[4], marginBottom: spacing[1]}]} />
+          </View>
+        </Screen>
+      </View>
+    )
+  },
+)
